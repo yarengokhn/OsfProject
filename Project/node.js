90ae6d6378a44f43cfadc51177a2c6e8cbd96dfc
yaren_gokhn <yarengokhn@gmail.com>
2022-09-03 12:43:20 +0300

    first commit

---
+++ Project/node.js
@@ -0,0 +1,48 @@
+const dropdowns =document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+   const select =dropdown.querySelector('.select')
+   const caret =dropdown.querySelector('.caret')
+   const menu =dropdown.querySelector('.menu')
+   const options =dropdown.querySelectorAll('.menu li')
+   const selected =dropdown.querySelector('.selected')
+
+})
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
